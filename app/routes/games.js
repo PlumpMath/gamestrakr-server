b5f949cdaddc0ee701b382e99e92f4341dc5a654
Evan Rubinton <evanrubinton@gmail.com>
2016-05-10 17:14:05 -0400

    add readme, remove unused error fn

--- app/routes/games.js
+++ app/routes/games.js
@@ -2,28 +2,14 @@ const express = require("express"),
   router = express.Router(),
   GAMES_COLLECTION = "games";
 
-// Generic error handler used by all endpoints.
-function handleError(res, reason, message, code) {
-  console.log("ERROR: " + reason);
-  res.status(code || 500).json({"error": message});
-}
-
 module.exports = function(db){
 
   router.get('/', function(req, res){
     db.collection(GAMES_COLLECTION).find({}).toArray(function(err, docs) {
-      if (err) {
-        handleError(res, err.message, "Failed to get games.");
-      } else {
-        res.status(200).json(docs);
-      }
+      res.status(200).json(docs);
     });
   });
 
-  router.get('/test', function(req, res){
-    res.send('heheheh');
-  });
-
   return router;
-}
+};
 
---
+++ readme.md
@@ -0,0 +1,8 @@
+# GamerLyfe Server
+## Dev Notes
+### Make sure to have .env file
+MONGODB_URI='mongodb://localhost:27017/gamer-lyfe'
+NODE_ENV='development'
+### Start mongodb sever locally
+mongod --dbpath=./data
+

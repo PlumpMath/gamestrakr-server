628add0e28c6815c76ef1706da5b95664b94bdf9
Evan Rubinton <evanrubinton@gmail.com>
2016-05-12 18:12:15 -0400

    add recent releases route, change home route to upcoming

--- app/routes/games.js
+++ app/routes/games.js
@@ -2,7 +2,6 @@
 // TODO: give user accounts ability to add currently playing
 // TODO: cache giantbomb requests(superagent-cache, redis?)
 
-
 const express = require("express")
   request = require("superagent"),
   _ = require("lodash"),
@@ -14,7 +13,7 @@ require('dotenv').load();
 
 module.exports = function(db){
 
-  router.get('/', function(req, res){
+  router.get('/recent_releases', function(req, res){
     const todayDateTime = moment().format('YYYY-MM-DD');
     request
       .get("http://www.giantbomb.com/api/games")
@@ -24,6 +23,27 @@ module.exports = function(db){
         field_list: "name,original_release_date,image,deck,description,platforms,site_detail_url",
         sort: "original_release_date:desc",
         filter: `original_release_date:1700-01-01|${todayDateTime}`,
+        limit: 50
+      })
+      .end(function(err, data){
+        if (err) {
+          console.log(err, "err");
+        } else if (_.has(data, "res.body.results")){
+          res.status(200).json(data.res.body.results);
+        }
+      });
+  });
+
+  router.get('/upcoming', function(req, res){
+    const tmrDateTime = moment().add(1, 'days').format('YYYY-MM-DD');
+    request
+      .get("http://www.giantbomb.com/api/games")
+      .query({
+        api_key: process.env.GIANTBOMB_KEY,
+        format: "json",
+        field_list: "name,expected_release_day,expected_release_month,expected_release_quarter,expected_release_year,image,deck,description,platforms,site_detail_url",
+        sort: "original_release_date:asc",
+        filter: `original_release_date:${tmrDateTime}|2018-12-00`,
         limit: 50
       })
       .end(function(err, data){

565f9eb7cfdbe19047ca04d13a0aa8c367c3561e
Evan Rubinton <evanrubinton@gmail.com>
2016-05-13 20:06:11 -0400

    update handling of upcoming and recent release games fetching

--- app/routes/games.js
+++ app/routes/games.js
@@ -3,7 +3,7 @@
 // TODO: cache giantbomb requests(superagent-cache, redis?)
 
 const express = require("express")
-  request = require("superagent"),
+request = require("superagent"),
   _ = require("lodash"),
   moment = require('moment'),
   router = express.Router(),
@@ -11,48 +11,56 @@ const express = require("express")
 
 require('dotenv').load();
 
+function fetchUpcomingReleases(res){
+  const tmrDateTime = moment().add(1, 'days').format('YYYY-MM-DD');
+  request
+    .get("http://www.giantbomb.com/api/games")
+    .query({
+      api_key: process.env.GIANTBOMB_KEY,
+      format: "json",
+      field_list: "name,expected_release_day,expected_release_month,expected_release_quarter,expected_release_year,image,deck,description,platforms,site_detail_url",
+      sort: "original_release_date:asc",
+      filter: `original_release_date:${tmrDateTime}|2018-12-00`,
+      limit: 50
+    })
+    .end(function(err, data){
+      if (err) {
+        console.log(err, "err");
+      } else if (_.has(data, "res.body.results")){
+        res.status(200).json(data.res.body.results);
+      }
+    });
+}
+
+function fetchRecentReleases(res){
+  const todayDateTime = moment().format('YYYY-MM-DD');
+  request
+    .get("http://www.giantbomb.com/api/games")
+    .query({
+      api_key: process.env.GIANTBOMB_KEY,
+      format: "json",
+      field_list: "name,original_release_date,image,deck,description,platforms,site_detail_url",
+      sort: "original_release_date:desc",
+      filter: `original_release_date:1700-01-01|${todayDateTime}`,
+      limit: 50
+    })
+    .end(function(err, data){
+      if (err) {
+        console.log(err, "err");
+      } else if (_.has(data, "res.body.results")){
+        res.status(200).json(data.res.body.results);
+      }
+    });
+}
+
 module.exports = function(db){
 
-  router.get('/recent_releases', function(req, res){
-    const todayDateTime = moment().format('YYYY-MM-DD');
-    request
-      .get("http://www.giantbomb.com/api/games")
-      .query({
-        api_key: process.env.GIANTBOMB_KEY,
-        format: "json",
-        field_list: "name,original_release_date,image,deck,description,platforms,site_detail_url",
-        sort: "original_release_date:desc",
-        filter: `original_release_date:1700-01-01|${todayDateTime}`,
-        limit: 50
-      })
-      .end(function(err, data){
-        if (err) {
-          console.log(err, "err");
-        } else if (_.has(data, "res.body.results")){
-          res.status(200).json(data.res.body.results);
-        }
-      });
-  });
-
-  router.get('/upcoming_releases', function(req, res){
-    const tmrDateTime = moment().add(1, 'days').format('YYYY-MM-DD');
-    request
-      .get("http://www.giantbomb.com/api/games")
-      .query({
-        api_key: process.env.GIANTBOMB_KEY,
-        format: "json",
-        field_list: "name,expected_release_day,expected_release_month,expected_release_quarter,expected_release_year,image,deck,description,platforms,site_detail_url",
-        sort: "original_release_date:asc",
-        filter: `original_release_date:${tmrDateTime}|2018-12-00`,
-        limit: 50
-      })
-      .end(function(err, data){
-        if (err) {
-          console.log(err, "err");
-        } else if (_.has(data, "res.body.results")){
-          res.status(200).json(data.res.body.results);
-        }
-      });
+  router.get('/', function(req, res){
+    if(req.params.games_type == 'upcoming'){
+      fetchUpcomingReleases(res);
+    } else {
+      fetchRecentReleases(res);
+    }
   });
 
   return router;
